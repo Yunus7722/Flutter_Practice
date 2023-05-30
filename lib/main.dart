4b3157242034d6e823057ec9714a848cde3c74c0
codeNurture023 <118299570+Yunus7722@users.noreply.github.com>
2023-05-30 16:30:04 +0530

    chapter 2 First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Awesome App", home: HomePage()));
+  runApp(
+    MaterialApp(
+      title: "Awesome App",
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+    ),
+  );
 }
 
 class HomePage extends StatelessWidget {
@@ -11,130 +19,51 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Align(
-        alignment: Alignment.center,
-        child: Container(
-          // alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Container(
-              // width: MediaQuery.of(context).size.width,
-              // height: MediaQuery.of(context).size.height,
-               width:200,
-              height: 500,
-              color: Colors.teal,
-             
-          
-          
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                
-                
-                children: <Widget>[
-                  Container(
-                padding: const EdgeInsets.all(8),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-              
-                //  color: Colors.teal,
-                gradient: LinearGradient(colors: [Colors.yellow, Colors.red]),
-                //  shape: BoxShape.circle,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 5,
-                    offset: Offset(10, 15),
-                    // spreadRadius: 10
-                  )
-                ]),
-              
-                child: Text(
-              "Hello World",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
-                ),
-              
-                //  color: Colors.yellow,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-              
-                //  color: Colors.teal,
-                gradient: LinearGradient(colors: [Colors.yellow, Colors.red]),
-                //  shape: BoxShape.circle,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                boxShadow: [  
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 5,
-                    offset: Offset(10, 15),
-                    // spreadRadius: 10
-                  )
-                ]),
-              
-                child: Text(
-              "Hello World",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
-                ),
-              
-                //  color: Colors.yellow,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-              
-                //  color: Colors.teal,
-                gradient: LinearGradient(colors: [Colors.yellow, Colors.red]),
-                //  shape: BoxShape.circle,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 5,
-                    offset: Offset(10, 15),
-                    // spreadRadius: 10
-                  )
-                ]),
-              
-                child: Text(
-              "Hello World",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),
-                ),
-              
-                //  color: Colors.yellow,
-                width: 100,
-                height: 100,
-              ),
-              
-                ],
-              ),
-            ),
-          ),
-        ),
+      body: Container(child: Container()),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color.fromARGB(167, 5, 223, 223),
+        child: Icon(Icons.add_a_photo),
+        // mini: true,
       ),
+      drawer: Drawer(
+          backgroundColor: Color.fromARGB(253, 255, 255, 255),
+          width: 250,
+          // shape: CircleBorder(),
+          elevation: 10,
+          child: ListView(
+            padding: EdgeInsets.all(0),
+            children: <Widget>[
+              /*Custom Drawer Header*/
+              // DrawerHeader(child: Text("Hello World ",style:   TextStyle(
+              //   color: Colors.white,fontSize: 25
+              // ),),
+
+              // decoration: BoxDecoration(color: Colors.teal[600]),),
+              UserAccountsDrawerHeader(
+                accountName: Text("Kaizuku"),
+                accountEmail: Text("Kaizuku123@gmail.com"),
+                // currentAccountPicture: Image.network(
+                //     "https://lh3.googleusercontent.com/Ryg5ih-fOWbpEGDpbJYJz5RTq3_28Tvo2h3JQRCdiz16lw5ghitDMN6hcWA57g0d6_8VOqQYcCemRBOfZGQA64ZKkFOn71zuF1LDsXwCh4ywF-_z6VqxbBqmbQvK_-PWfZcztxDd1WDTEEUHX6GdP60"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage("https://lh3.googleusercontent.com/Ryg5ih-fOWbpEGDpbJYJz5RTq3_28Tvo2h3JQRCdiz16lw5ghitDMN6hcWA57g0d6_8VOqQYcCemRBOfZGQA64ZKkFOn71zuF1LDsXwCh4ywF-_z6VqxbBqmbQvK_-PWfZcztxDd1WDTEEUHX6GdP60"),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Account"),
+                subtitle: Text("Personal"),
+                trailing: Icon(Icons.edit),
+              ),
+              ListTile(
+                leading: Icon(Icons.email),
+                title: Text("Email ID"),
+                subtitle: Text("admin@gmail.com"),
+                trailing: Icon(Icons.send),
+              )
+            ],
+          )),
     );
   }
 }
